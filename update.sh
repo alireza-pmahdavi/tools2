a22626deb6e4bdbae69b0c2e68ee7d451d786361
Alireza Pourmahdavi <arzpmv@gmail.com>
2024-06-09 13:53:46 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 
 curl -sSfL https://raw.githubusercontent.com/aleksibovellan/opnsense-suricata-nmaps/main/local.rules -o /etc/suricata/rules/nmap.rules
 chmod 644 /etc/suricata/rules/nmap.rules
+sed -i '/#Malware\/trojan oriented rules/a\ - nmap.rules' /etc/suricata/suricata.yaml
+
+
 
 curl -sSfL https://raw.githubusercontent.com/alireza-pmahdavi/tools2/main/active-response.tar.gz -o /var/ossec/active-response/bin/active-response.tar.gz
 tar xvzf /var/ossec/active-response/bin/active-response.tar.gz -C /var/ossec/active-response/bin/
