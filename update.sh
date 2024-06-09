2a6edba6ad02428af08af86179c9c42d21a7030b
Alireza Pourmahdavi <arzpmv@gmail.com>
2024-06-09 13:33:43 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,15 @@
 #/bin/bash
 # Version: 1.0.0
+
+curl -sSfL https://raw.githubusercontent.com/aleksibovellan/opnsense-suricata-nmaps/main/local.rules -o /etc/suricata/rules/nmap.rules
+chmod 644 /etc/suricata/rules/nmap.rules
+
+curl -sSfL https://raw.githubusercontent.com/alireza-pmahdavi/tools2/main/active-response.tar.gz -o /var/ossec/var/ossec/active-response/bin/active-response.tar.gz
+tar xvzf /var/ossec/var/ossec/active-response/bin/active-response.tar.gz
+
+chmod 644 /etc/suricata/rules/block-*
+
+suricata-update
+systemctl restart suricata
+
+systemctl restart wazuh-agent.service
